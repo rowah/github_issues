364cac1ee5f4ef36fb20cda404f5c93929c8b3aa
Rowa <jrowah@gmail.com>
2023-02-23 12:27:38 +0300

    creats help arg handler func

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -7,7 +7,17 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
+  end
+
+  def process(:help) do
+    IO.puts("""
+    usage: issues <user> <project> [count | #{@default_count}]
+    """)
+
+    System.halt(0)
   end
 
   @doc """
